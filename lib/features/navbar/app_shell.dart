662bad4b92e732b0b00bdfff6af538e1a02b9821
Amisha Basnet <amishaaaaaa045@gmail.com>
2026-06-10 18:13:43 +0545

    nav bar

---
+++ lib/features/navbar/app_shell.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pustakalaya/features/home/presentation/screens/home_screen.dart';
+
+import 'nav_provider.dart';
+import 'pustakalaya_nav_bar.dart';
+
+class AppShell extends ConsumerWidget {
+  const AppShell({super.key});
+
+  static const _screens = [
+    HomeScreen(),
+    WishlistScreen(),
+    CartScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final activeTab = ref.watch(activeTabProvider);
+
+    return Scaffold(
+      body: IndexedStack(
+        index: activeTab,
+        children: _screens,
+      ),
+      bottomNavigationBar: PustakalayaNavBar(
+        currentIndex: activeTab,
+        onTap: (i) => ref.read(activeTabProvider.notifier).state = i,
+      ),
+    );
+  }
+}
