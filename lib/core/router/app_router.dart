db8fe7828330fcec9758b8682237dc4adbc521ed
Amisha Basnet <amishaaaaaa045@gmail.com>
2026-06-13 23:57:51 +0545

    code

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:pustakalaya/features/auth/presentation/screens/sign_in_screen.dart';
 import 'package:pustakalaya/features/auth/presentation/screens/sign_up_screen.dart';
+import 'package:pustakalaya/features/book_detail/presentation/screens/book_detail_screen.dart';
 import 'package:pustakalaya/features/navbar/app_shell.dart';
 import 'package:pustakalaya/features/onboarding/presentation/pages/onboarding_screen.dart';
 import 'package:pustakalaya/features/orders/presentation/screens/my_orders_screen.dart';
@@ -15,22 +16,18 @@ class AppRouter {
   static const String signIn = '/sign-in';
   static const String home = '/home';
   static const String myOrders = '/my-orders';
+  static const String bookDetail = '/book-detail';
 
   static final router = GoRouter(
     initialLocation: splash,
     routes: [
-      GoRoute(path: splash, builder: (context, state) => const SplashScreen()),
-      GoRoute(
-        path: onboarding,
-        builder: (context, state) => const OnboardingScreen(),
-      ),
-      GoRoute(path: signUp, builder: (context, state) => const SignUpScreen()),
-      GoRoute(path: signIn, builder: (context, state) => const SignInScreen()),
-      GoRoute(path: home, builder: (context, state) => const AppShell()),
-      GoRoute(
-        path: myOrders,
-        builder: (context, state) => const MyOrdersScreen(),
-      ),
+      GoRoute(path: splash, builder: (_, __) => const SplashScreen()),
+      GoRoute(path: onboarding, builder: (_, __) => const OnboardingScreen()),
+      GoRoute(path: signUp, builder: (_, __) => const SignUpScreen()),
+      GoRoute(path: signIn, builder: (_, __) => const SignInScreen()),
+      GoRoute(path: home, builder: (_, __) => const AppShell()),
+      GoRoute(path: myOrders, builder: (_, __) => const MyOrdersScreen()),
+      GoRoute(path: bookDetail, builder: (_, __) => const BookDetailScreen()),
     ],
   );
 }
